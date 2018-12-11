cba62b0ea53385deb713282f4ccfbc86b7634bb0
nvms <jon@pye.rs>
2018-12-11 15:31:18 -0800

    /src/Layout moved to /src/Grid

---
+++ src/Grid/index.js
@@ -0,0 +1,1 @@
+export { default as VaGrid } from './install'
---
+++ src/Grid/install.js
@@ -0,0 +1,7 @@
+import VaColumn from './VaColumn.vue'
+import VaRow from './VaRow.vue'
+
+export default Vue => {
+  Vue.component(VaColumn.name, VaColumn)
+  Vue.component(VaRow.name, VaRow)
+}
