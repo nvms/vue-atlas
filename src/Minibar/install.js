fa4231271616bb4d3cf9dd5f55c5a7043c1fda88
nvms <jon@pye.rs>
2018-12-11 15:57:02 -0800

    [fix] forgot to include VaMinibarItem

--- src/Minibar/install.js
+++ src/Minibar/install.js
@@ -1,5 +1,7 @@
 import VaMinibar from './VaMinibar.vue'
+import VaMinibarItem from './VaMinibarItem.vue'
 
 export default Vue => {
   Vue.component(VaMinibar.name, VaMinibar)
+  Vue.component(VaMinibarItem.name, VaMinibarItem)
 }
