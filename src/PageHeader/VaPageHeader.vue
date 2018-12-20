4e2bdad4ebd6000fbbdeabbdb380479ad7655a4c
Marvin Rabe <marvin.rabe@gmail.com>
2018-12-20 20:26:58 +0100

    Hide wrapper in page header component.

--- src/PageHeader/VaPageHeader.vue
+++ src/PageHeader/VaPageHeader.vue
@@ -1,45 +1,56 @@
 <template>
-  <div :class="`${prefixCls}-page-header`">
+    <div :class="`${prefixCls}-page-header`">
 
-    <div :class="`${prefixCls}-page-header-breadcrumb-wrapper`">
-      <div :class="`${prefixCls}-page-header-breadcrumb-container`">
-        <slot name="breadcrumb">&nbsp;</slot>
-      </div>
+        <div v-if="hasBreadcrumb" :class="`${prefixCls}-page-header-breadcrumb-wrapper`">
+            <div :class="`${prefixCls}-page-header-breadcrumb-container`">
+                <slot name="breadcrumb">&nbsp;</slot>
+            </div>
+        </div>
+
+        <div :class="`${prefixCls}-page-header-title-wrapper`">
+            <div :class="`${prefixCls}-page-header-title-container`">
+                <h1>
+                    <slot name="title"></slot>
+                </h1>
+                <h2 v-if="hasSubtitle">
+                    <slot name="subtitle"></slot>
+                </h2>
+            </div>
+
+            <div v-if="hasActions" :class="`${prefixCls}-page-header-actions-wrapper`">
+                <slot name="actions"></slot>
+            </div>
+        </div>
+
+        <div v-if="hasBottom" :class="`${prefixCls}-page-header-bottom-wrapper`">
+            <slot name="bottom">&nbsp;</slot>
+        </div>
+
     </div>
-
-    <div :class="`${prefixCls}-page-header-title-wrapper`">
-      <div :class="`${prefixCls}-page-header-title-container`">
-        <h1>
-          <slot name="title"></slot>
-        </h1>
-      </div>
-
-      <div :class="`${prefixCls}-page-header-actions-wrapper`">
-        <slot name="actions"></slot>
-      </div>
-    </div>
-
-    <div :class="`${prefixCls}-page-header-subtitle-container`">
-      <h2>
-        <slot name="subtitle"></slot>
-      </h2>
-    </div>
-
-    <div :class="`${prefixCls}-page-header-bottom-wrapper`">
-      <slot name="bottom">&nbsp;</slot>
-    </div>
-
-  </div>
 </template>
 
 <script>
-export default {
-  name: 'VaPageHeader',
-  props: {
-    prefixCls: {
-      type: String,
-      default: 'va'
+  export default {
+    name: 'VaPageHeader',
+    props: {
+      prefixCls: {
+        type: String,
+        default: 'va'
+      }
+    },
+    computed: {
+      hasActions() {
+        return !!this.$slots['actions']
+      },
+      hasBottom() {
+        return !!this.$slots['bottom']
+      },
+      hasBreadcrumb() {
+        return !!this.$slots['breadcrumb']
+      },
+      hasSubtitle() {
+        return !!this.$slots['subtitle']
+      }
     }
   }
-}
 </script>
---
+++ tests/unit/VaPageHeader.spec.js
@@ -0,0 +1,81 @@
+import {mount, shallowMount} from '@vue/test-utils'
+import VaPageHeader from '@/PageHeader/VaPageHeader.vue'
+
+describe('VaPageHeader.vue', () => {
+  it('renders correctly', () => {
+    const wrapper = mount(VaPageHeader)
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('does not render empty breadcrumb wrapper', () => {
+    const wrapper = shallowMount(VaPageHeader, {
+      slots: {}
+    })
+
+    expect(wrapper.find('.va-page-header-breadcrumb-container').exists()).toBeFalsy()
+  })
+
+  it('renders breadcrumb wrapper', () => {
+    const wrapper = shallowMount(VaPageHeader, {
+      slots: {
+        breadcrumb: '<strong>Test</strong>'
+      }
+    })
+
+    expect(wrapper.find('.va-page-header-breadcrumb-container').exists()).toBeTruthy()
+  })
+
+  it('does not render empty bottom wrapper', () => {
+    const wrapper = shallowMount(VaPageHeader, {
+      slots: {}
+    })
+
+    expect(wrapper.find('.va-page-header-bottom-wrapper').exists()).toBeFalsy()
+  })
+
+  it('renders bottom wrapper', () => {
+    const wrapper = shallowMount(VaPageHeader, {
+      slots: {
+        bottom: '<strong>Test</strong>'
+      }
+    })
+
+    expect(wrapper.find('.va-page-header-bottom-wrapper').exists()).toBeTruthy()
+  })
+
+  it('does not render empty actions wrapper', () => {
+    const wrapper = shallowMount(VaPageHeader, {
+      slots: {}
+    })
+
+    expect(wrapper.find('.va-page-header-actions-wrapper').exists()).toBeFalsy()
+  })
+
+  it('renders actions wrapper', () => {
+    const wrapper = shallowMount(VaPageHeader, {
+      slots: {
+        actions: '<strong>Test</strong>'
+      }
+    })
+
+    expect(wrapper.find('.va-page-header-actions-wrapper').exists()).toBeTruthy()
+  })
+
+  it('does not render empty subtitle wrapper', () => {
+    const wrapper = shallowMount(VaPageHeader, {
+      slots: {}
+    })
+
+    expect(wrapper.find('h2').exists()).toBeFalsy()
+  })
+
+  it('renders subtitle wrapper', () => {
+    const wrapper = shallowMount(VaPageHeader, {
+      slots: {
+        subtitle: '<strong>Test</strong>'
+      }
+    })
+
+    expect(wrapper.find('h2').exists()).toBeTruthy()
+  })
+})
