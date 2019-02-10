41a4790553751233c82a071aace29d3da94f869b
Jonathan Pyers <jon@pye.rs>
2019-02-09 16:06:28 -0800

    [refactor] cleanup publish.sh

--- publish.sh
+++ publish.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-yarn run build
+# build it
+yarn build
+
+# remove the old stuff
 rm -rf ~/vue-atlas-npm
+
+# 
 mkdir ~/vue-atlas-npm
+
+# copy the important stuff
 cp -r src ~/vue-atlas-npm
 cp -r dist ~/vue-atlas-npm
+
+# copy over package spec, license and readme
 cp package.json ~/vue-atlas-npm
 cp LICENSE ~/vue-atlas-npm
 cp README.md ~/vue-atlas-npm
 
-echo "ready to publish?"
-echo "cd ~/vue-atlas-npm && npm publish"
+# remove the example component
+rm ~/vue-atlas-npm/src/App.vue
 
 exit 0
