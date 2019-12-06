a37d0ade9ee0b69f5b22b6e4c6013eb3aa937153
Jonathan Pyers <jon@pye.rs>
2019-12-06 15:01:26 -0800

    fix(vue config): data should be prependData

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `@import "@/variables.scss";`
+        prependData: `@import "@/variables.scss";`
       }
     }
   }
